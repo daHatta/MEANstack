1cc9d64b0a90f00ee953ed18029505db1f403066
Heiko John <healjo@hotmail.com>
2020-09-01 18:15:29 +0200

    test

--- app_server/controllers/main.js
+++ app_server/controllers/main.js
@@ -1,6 +1,6 @@
 /* GET home page. */
 const index = (req, res) => {
-    res.render('index', { title: 'ExprHelloess' });
+    res.render('index', { title: 'Hello Express' });
 }
 
 module.exports = {
